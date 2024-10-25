e7f6100a3481af46e1e60db71f184d5f0f3e900c
devCaiodias <caiodiasmartins12@gmail.com>
2024-10-25 14:17:01 -0300

    Criando o user Reducer e Acessando dados de um Reducer com useSelector

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,5 +1,17 @@
-const userReducer = () => {
-    
+
+const initialState = {
+    currentUser: 1000,
+}
+
+const userReducer = (state = initialState, action) => {
+    if (action.type === 'user/login') {
+        return {
+            ...state,
+            currentUser: 100
+        }
+    }
+
+    return state;
 }
 
 export default userReducer;
