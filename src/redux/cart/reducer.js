d38708e02ad54a5256e590e6899349f0af04cbe0
devCaiodias <caiodiasmartins12@gmail.com>
2024-10-26 18:36:59 -0300

    Boa pratica: Logicas devem ficar dentro do Reducer & Aprimorando a edição de um produto no carrinho

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -8,9 +8,24 @@ const inicialState = {
 const cartReducer = (state = inicialState, action) => {
     switch (action.type) {
         case CartActionTypes.ADDPRODUCT:
+            //  verificar se o produto já está no carrinho 
+            const productIsAlreadyInCart = state.cartProduct.some(
+                (product) => product.id === action.payload.id
+            )
+
+            // se ele tiver no carrinho, aumentar sua quantidade em 1
+            if (productIsAlreadyInCart) {
+                return {
+                    ...state,
+                    cartProduct: state.cartProduct.map((product) => product.id === action.payload.id ? {...product, quantity: product.quantity + 1}: product),
+                }
+            }
+
+            //  se ele n tiver adicionar ele no carrinho
+
             return {
                 ...state,
-                cartProduct: [...state.cartProduct, action.payload]
+                cartProduct: [...state.cartProduct, {...action.payload, quantity: 1}]
             }
     
         default:
